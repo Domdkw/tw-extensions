4f79448b59c7ffba8f03f452bb75e989b7c4a0ac
Domdkw <domdkw@qq.com>
2026-04-04 11:57:11 +0800

    feat(国际化): 添加转码工具的国际化支持
    为转码工具添加多语言支持，包括德语、芬兰语、意大利语、日语、挪威语、俄语和简体中文
    更新区块文本以使用翻译系统，移除硬编码的默认值

--- src/main.js
+++ src/main.js
@@ -1,4 +1,78 @@
 //import CryptoJS from 'crypto-js'
+//Name: Transcoding
+//Description: a tool to decode and encode text
+//Author: Domdkw
+//Version: 1.0.0
+//License: MIT
+
+/* generated l10n code */
+Scratch.translate.setup({
+    "de":{
+        "_Transcoding":"Transkodierung",
+        "_b64encode":"Base64 kodieren [text]",
+        "_base64 decode [text]":"Base64 dekodieren [text]",
+        "_url encode [text]":"URL kodieren [text]",
+        "_url decode [text]":"URL dekodieren [text]",
+        "_url encode component [text]":"URL-Komponente kodieren [text]",
+        "_url decode component [text]":"URL-Komponente dekodieren [text]"
+    },
+    "fi":{
+        "_Transcoding":"Transkoodaus",
+        "_b64encode":"Base64-koodaa [text]",
+        "_base64 decode [text]":"Base64-purkaa [text]",
+        "_url encode [text]":"URL-koodaa [text]",
+        "_url decode [text]":"URL-purkaa [text]",
+        "_url encode component [text]":"URL-komponentti koodaa [text]",
+        "_url decode component [text]":"URL-komponentti purkaa [text]"
+    },
+    "it":{
+        "_Transcoding":"Transcodifica",
+        "_b64encode":"Codifica Base64 [text]",
+        "_base64 decode [text]":"Decodifica Base64 [text]",
+        "_url encode [text]":"Codifica URL [text]",
+        "_url decode [text]":"Decodifica URL [text]",
+        "_url encode component [text]":"Codifica componente URL [text]",
+        "_url decode component [text]":"Decodifica componente URL [text]"
+    },
+    "ja":{
+        "_Transcoding":"トランスコーディング",
+        "_b64encode":"Base64エンコード [text]",
+        "_base64 decode [text]":"Base64デコード [text]",
+        "_url encode [text]":"URLエンコード [text]",
+        "_url decode [text]":"URLデコード [text]",
+        "_url encode component [text]":"URLコンポーネントエンコード [text]",
+        "_url decode component [text]":"URLコンポーネントデコード [text]"
+    },
+    "nb":{
+        "_Transcoding":"Transkoding",
+        "_b64encode":"Base64-kode [text]",
+        "_base64 decode [text]":"Base64-dekode [text]",
+        "_url encode [text]":"URL-kode [text]",
+        "_url decode [text]":"URL-dekode [text]",
+        "_url encode component [text]":"URL-komponent kode [text]",
+        "_url decode component [text]":"URL-komponent dekode [text]"
+    },
+    "ru":{
+        "_Transcoding":"Транскодирование",
+        "_b64encode":"Base64 кодировать [text]",
+        "_base64 decode [text]":"Base64 декодировать [text]",
+        "_url encode [text]":"URL кодировать [text]",
+        "_url decode [text]":"URL декодировать [text]",
+        "_url encode component [text]":"URL-компонент кодировать [text]",
+        "_url decode component [text]":"URL-компонент декодировать [text]"
+    },
+    "zh-cn":{
+        "_Transcoding":"转码工具",
+        "_b64encode":"base64 编码 [text]",
+        "_base64 decode [text]":"base64 解码 [text]",
+        "_url encode [text]":"URL 编码 [text]",
+        "_url decode [text]":"URL 解码 [text]",
+        "_url encode component [text]":"URL 组件编码 [text]",
+        "_url decode component [text]":"URL 组件解码 [text]"
+    }
+});
+/* end generated l10n code */
+
 
 (function (Scratch){
     "use strict";
@@ -6,8 +80,8 @@
     class Transcoding{
         getInfo(){
             return {
-                id: 'dmTranscoding',
-                name: 'transcoding',
+                id: 'Transcoding',
+                name: Scratch.translate('Transcoding'),
                 version: '1.0.0',
                 author: 'Domdkw',
                 description: 'a tool to decode and encode text',
@@ -18,7 +92,7 @@
                     {
                         opcode: 'b64encode',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: 'base64 encode [text]',
+                        text: Scratch.translate('b64encode'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
@@ -29,60 +103,55 @@
                     {
                         opcode: 'b64decode',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: 'base64 decode [text]',
+                        text: Scratch.translate('base64 decode [text]'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'aGVsbG8=',
-                                // base64 decode 'hello'
                             }
                         }
                     },
                     {
                         opcode: 'urlencode',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: 'url encode [text]',
+                        text: Scratch.translate('url encode [text]'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'a=1 & b=2',
-                                // url encode 'a=1%20&%20b=2'
                             }
                         }
                     },
                     {
                         opcode: 'urldecode',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: 'url decode [text]',
+                        text: Scratch.translate('url decode [text]'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'a=1%20&%20b=2',
-                                // url decode 'a=1 & b=2'
                             }
                         }
                     },
                     {
                         opcode: 'urlencodeComponent',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: ' url encode component [text]',
+                        text: Scratch.translate('url encode component [text]'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'a=1&b=2',
-                                // url encode component 'a%3D1%26b%3D2'
                             }
                         }
                     },
                     {
                         opcode: 'urldecodeComponent',
                         blockType: Scratch.BlockType.REPORTER,
-                        text: ' url decode component [text]',
+                        text: Scratch.translate('url decode component [text]'),
                         arguments:{
                             text: {
                                 type: Scratch.ArgumentType.STRING,
                                 defaultValue: 'a%3D1%26b%3D2',
-                                // url decode component 'a=1&b=2',
                             }
                         }
                     },
